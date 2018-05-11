758d892ae8e37d147ec1a598616c95ba13d03336
jwong955 <35752692+jwong955@users.noreply.github.com>
2018-05-11 16:41:10 -0700

    Update Map.cpp

--- Src/Map.cpp
+++ Src/Map.cpp
@@ -16,10 +16,15 @@ void Map::LoadMap(std::string path, int sizeX, int sizeY) {
 	std::fstream mapFile;
 	mapFile.open(path);
 
+	int srcX, srcY;
+
 	for (int y = 0; y < sizeY; y++) {
 		for (int x = 0; x < sizeX; x++) {
 			mapFile.get(tile);
-			Game::AddTile(atoi(&tile), x * 32, y * 32);
+			srcY = atoi(&tile) * 32;
+			mapFile.get(tile);
+			srcX = atoi(&tile) * 32;
+			Game::AddTile(srcX, srcY, x * 64, y * 64);
 			mapFile.ignore();
 		}
 	}

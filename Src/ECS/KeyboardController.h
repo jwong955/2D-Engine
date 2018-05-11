2dd70a1d84c652d2354c4de5a42c1dc199180268
jwong955 <35752692+jwong955@users.noreply.github.com>
2018-05-11 16:26:28 -0700

    Update KeyboardController.h

--- Src/ECS/KeyboardController.h
+++ Src/ECS/KeyboardController.h
@@ -7,9 +7,10 @@
 class KeyboardController :public Component {
 public:
 	TransformComponent *transform;
-
+	SpriteComponent* sprite;
 	void init()override {
 		transform = &entity->getComponent<TransformComponent>();
+		sprite = &entity->getComponent<SpriteComponent>();
 	}
 
 	void update() override {
@@ -18,15 +19,20 @@ public:
 			switch (Game::event.key.keysym.sym) {
 			case SDLK_w:
 				transform->velocity.y = -1;
+				sprite->play("Walk");
 				break;
 			case SDLK_a:
 				transform->velocity.x = -1;
+				sprite->play("Walk");
+				sprite->spriteFlip = SDL_FLIP_HORIZONTAL;
 				break;
 			case SDLK_d:
 				transform->velocity.x = 1;
+				sprite->play("Walk");
 				break;
 			case SDLK_s:
 				transform->velocity.y = 1;
+				sprite->play("Walk");
 				break;
 			default:
 				break;
@@ -37,16 +43,23 @@ public:
 			switch (Game::event.key.keysym.sym) {
 			case SDLK_w:
 				transform->velocity.y = 0;
+				sprite->play("Idle");
 				break;
 			case SDLK_a:
 				transform->velocity.x = 0;
+				sprite->play("Idle");
+				sprite->spriteFlip = SDL_FLIP_NONE;
 				break;
 			case SDLK_d:
 				transform->velocity.x = 0;
+				sprite->play("Idle");
 				break;
 			case SDLK_s:
 				transform->velocity.y = 0;
+				sprite->play("Idle");
 				break;
+			case SDLK_ESCAPE:
+				Game::isRunning = false;
 			default:
 				break;
 			}

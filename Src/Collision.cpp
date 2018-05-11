82ed44b52154b4d5edeb783bb8d271540b8091ad
jwong955 <35752692+jwong955@users.noreply.github.com>
2018-05-11 16:38:38 -0700

    Update Collision.cpp

--- Src/Collision.cpp
+++ Src/Collision.cpp
@@ -15,7 +15,7 @@ bool Collision::AABB(const SDL_Rect& rectA, const SDL_Rect& rectB) {
 
 bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& colB) {
 	if (AABB(colA.collider, colB.collider)) {
-		std::cout << colA.tag << "hit: " << colB.tag << std::endl;
+		//std::cout << colA.tag << "hit: " << colB.tag << std::endl;
 		return true;
 	}
 	else {

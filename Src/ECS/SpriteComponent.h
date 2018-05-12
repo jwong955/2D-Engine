1ba3579c354d68cb363c08dc9f4eec17131e5b37
jwong955 <35752692+jwong955@users.noreply.github.com>
2018-05-11 18:27:37 -0700

    Update SpriteComponent.h

--- Src/ECS/SpriteComponent.h
+++ Src/ECS/SpriteComponent.h
@@ -67,8 +67,8 @@ public:
 			srcRect.x = srcRect.w*static_cast<int>((SDL_GetTicks() / speed) % frames);
 		}
 		srcRect.y = animIndex * transform->height;
-		destRect.x = static_cast<int>(transform->position.x);
-		destRect.y = static_cast<int>(transform->position.y);
+		destRect.x = static_cast<int>(transform->position.x) - Game::camera.x;
+		destRect.y = static_cast<int>(transform->position.y) - Game::camera.y;
 		destRect.w = transform->width*transform->scale;
 		destRect.h = transform->height*transform->scale;
 	}

f6b710b2b895492f2765f28c991a4ef46b68e6f6
jturnquist276 <35823548+jturnquist276@users.noreply.github.com>
2018-05-12 23:06:57 -0700

    FIXED MAP BUG.
    I guess mac commands work slightly differently or something... I had to make sure it specifically only ignores anything that isn't alphanumerical.

--- Src/Map.cpp
+++ Src/Map.cpp
@@ -22,10 +22,14 @@ void Map::LoadMap(std::string path, int sizeX, int sizeY) {
 		for (int x = 0; x < sizeX; x++) {
 			mapFile.get(tile);
 			srcY = atoi(&tile) * 32;
+            //std::cout << tile;
 			mapFile.get(tile);
 			srcX = atoi(&tile) * 32;
+            //std::cout << tile << ",";
 			Game::AddTile(srcX, srcY, x * 64, y * 64);
-			mapFile.ignore();
+            while (!(std::isalnum(mapFile.peek())) && !mapFile.eof()) {
+                mapFile.ignore();
+            }
 		}
 	}
 	mapFile.close();
